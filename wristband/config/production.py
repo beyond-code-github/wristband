d5ef5d631c983dd5271567f3a8c60811be15118a
Alan Platt <alan.platt@digital.cabinet-office.gov.uk>
2015-07-17 16:00:58 +0100

    added check for https in config

--- wristband/config/production.py
+++ wristband/config/production.py
@@ -1,9 +1,17 @@
 # config.py
 
 import os
+import re
 
 __environment_list = os.getenv("ENVIRONMENTS").split(",")
-__environments = {e: {"jenkins_uri": os.getenv("ENVIRONMENT_{}_jenkins_uri".format(e).replace("-", "_"))} for e in __environment_list}
+__environments = {}
+
+for env in __environment_list:
+    environment_jenkins = os.getenv("ENVIRONMENT_{}_jenkins_uri".format(env).replace("-", "_"))
+    if not re.match('https.*', environment_jenkins):
+        print "WARNING: {} should be https".format(environment_jenkins)
+    else:
+        __environments[env] = {"jenkins_uri": os.getenv("ENVIRONMENT_{}_jenkins_uri".format(env).replace("-", "_"))}
 
 __pipelines_list = os.getenv("PIPELINES").split(",")
 __pipelines = {p: os.getenv("PIPELINE_{}".format(p).replace("-", "_")).split(",") for p in __pipelines_list}
