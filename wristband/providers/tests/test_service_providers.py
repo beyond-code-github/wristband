391b410c20446041642e9d8471f5acc97ec4d29b
Alessandro De Noia <alessandro.denoia@gmail.com>
2015-09-01 15:58:29 +0100

    wristband37 added jenkins provider tests

--- wristband/providers/tests/test_service_providers.py
+++ wristband/providers/tests/test_service_providers.py
@@ -1,14 +1,62 @@
+import mock
+
 from wristband.providers.service_providers import JenkinsServiceProvider
 
 @mock.patch('wristband.providers.service_providers.jenkins')
-def test_jenkins_provider_save_job_info(mocked_jenkins):
-    mocked_jenkins.get_job_info.return_value = {'nextBuildNumber': 5}
-    mocked_jenkins.get_build_info.return_value = {
+@mock.patch('wristband.providers.service_providers.Job')
+@mock.patch('wristband.providers.service_providers.App')
+@mock.patch.object(JenkinsServiceProvider, 'get_jenkins_server_config')
+def test_jenkins_provider_save_job_info_successful_case(mocked_get_jenkins_server_config,
+                                                        mocked_app_model,
+                                                        mocked_job_model,
+                                                        mocked_jenkins):
+    provider_under_test = JenkinsServiceProvider('foo', 'bar')
+    mocked_get_jenkins_server_config.return_value = {'username': 'john',
+                                                     'password': 'password',
+                                                     'uri': 'http://test.com'}
+
+    # it's easier to monkey patch the mocked jenkins server instance here
+    mocked_jenkins_server = mock.Mock()
+    mocked_jenkins_server.get_job_info.return_value = {'nextBuildNumber': 5}
+    mocked_jenkins_server.get_build_info.return_value = {
         'actions': [
             {
-                'paramenters': [
+                'parameters': [
                     {'name': 'APP_BUILD_NUMBER', 'value': '0.1.0'}
                 ]
             }
         ]
     }
+    provider_under_test.server = mocked_jenkins_server
+    provider_under_test.save_job_info('0.1.0')
+    assert mocked_job_model.save.called_once()
+
+
+@mock.patch('wristband.providers.service_providers.jenkins')
+@mock.patch('wristband.providers.service_providers.Job')
+@mock.patch('wristband.providers.service_providers.App')
+@mock.patch.object(JenkinsServiceProvider, 'get_jenkins_server_config')
+def test_jenkins_provider_save_job_info_failure_case(mocked_get_jenkins_server_config,
+                                                        mocked_app_model,
+                                                        mocked_job_model,
+                                                        mocked_jenkins):
+    provider_under_test = JenkinsServiceProvider('foo', 'bar')
+    mocked_get_jenkins_server_config.return_value = {'username': 'john',
+                                                     'password': 'password',
+                                                     'uri': 'http://test.com'}
+
+    # it's easier to monkey patch the mocked jenkins server instance here
+    mocked_jenkins_server = mock.Mock()
+    mocked_jenkins_server.get_job_info.return_value = {'nextBuildNumber': 5}
+    mocked_jenkins_server.get_build_info.return_value = {
+        'actions': [
+            {
+                'parameters': [
+                    {'name': 'APP_BUILD_NUMBER', 'value': '0.1.0'}
+                ]
+            }
+        ]
+    }
+    provider_under_test.server = mocked_jenkins_server
+    job_id = provider_under_test.save_job_info('0.2.0')
+    assert job_id is None
